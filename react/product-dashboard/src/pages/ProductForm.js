6db22254daf16f479feeba3df0dde7db7d98bc95
Colin Clarke <colin_clarke@hcl.com>
2021-08-20 12:34:14 -0400

    File structure

--- react/product-dashboard/src/pages/ProductForm.js
+++ react/product-dashboard/src/pages/ProductForm.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Header from '../components/Header';
 
 function ProductForm() {
 
     let {pid} = useParams();
 
     return (
-        <p>{typeof pid == 'undefined' ? 'New Product' : 'Edit Product '+pid}</p>
+        <div>
+            <Header/>
+            <p>{typeof pid == 'undefined' ? 'New Product' : 'Edit Product '+pid}</p>
+        </div>
     );
 }
 
