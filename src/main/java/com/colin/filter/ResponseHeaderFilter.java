e2d5de44b9ec620ee24fecdc64e851ac54b9ff3b
Genevieve Zola <genevieve_zola@hcl.com>
2021-08-23 17:50:45 -0400

    changed to restful controller

---
+++ src/main/java/com/colin/filter/ResponseHeaderFilter.java
@@ -0,0 +1,24 @@
+package com.colin.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class ResponseHeaderFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader(
+          "Allow-Access-Filter", "Application-Header");
+        chain.doFilter(request, response);
+
+	}
+
+}
