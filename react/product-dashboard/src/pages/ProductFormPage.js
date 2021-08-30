daa7a02f5f8d33830c62b6f76a10c0f382ae5ed4
Colin Clarke <colin_clarke@hcl.com>
2021-08-30 14:16:35 -0400

    Redirect on form submit and error handling

--- react/product-dashboard/src/pages/ProductFormPage.js
+++ react/product-dashboard/src/pages/ProductFormPage.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import ProductForm from '../components/ProductForm';
 import useFetch from 'react-fetch-hook';
 
 function ProductFormPage() {
     const {pid} = useParams();
+    const history = useHistory();
     const { isLoading, data, error } = useFetch(process.env.REACT_APP_BASE_API_URL+'/products/'+pid, {
         depends: [typeof pid !== 'undefined']
     });
 
     if (error) {
-        return <h1>uh oh</h1>;
+        history.push("/products");
     }
 
     return (
         <div>
             <Header/>
+            <br/>
             <div className='container col-md-5'>
                 <h2>{typeof pid == 'undefined' ? 'New Product' : 'Edit Product '+pid}</h2>
                 {!isLoading && <ProductForm pc={data} />}
