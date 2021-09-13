8e681d131b8d3ae5e42a98cdcd87a651132b24d4
Colin Clarke <colin_clarke@hcl.com>
2021-09-13 16:12:04 -0400

    Frontend stripe

--- react/src/components/CartContents.js
+++ react/src/components/CartContents.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function CartContents({cart, totalPrice}) {
 
-    cartItemList = [];
+    const cartItemList = [];
     for (let i in cart) {
         let cartItem = cart[i];
         cartItemList.push(
-            <tr>
+            <tr key={i}>
                 <th scope="row">{cartItem.product.name}</th>
                 <td>{cartItem.quantity}</td>
                 <td>{cartItem.product.price}</td>
@@ -16,22 +16,24 @@ function CartContents({cart, totalPrice}) {
     }
 
     return (
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Price</th>
-                <th scope="col">Subtotal</th>
-                </tr>
-            </thead>
-            <tbody>
-                {cartItemList}
-                <tr>
-                    {totalPrice}
-                </tr>
-            </tbody>
-        </table>
+        <div className="my-3">
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cartItemList}
+                </tbody>
+            </table>
+            <div className="d-flex justify-content-end">
+                <h3 style={{color: 'green', fontWeight: 'bold'}}>{totalPrice}</h3>
+            </div>
+        </div>
     )
 }
 
--- react/src/pages/CartPage.js
+++ react/src/pages/CartPage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import useFetch from 'react-fetch-hook';
 import {useHistory} from 'react-router-dom';
 import CartContents from '../components/CartContents';
+import Header from '../components/Header';
 
 function CartPage() {
 
@@ -14,21 +15,30 @@ function CartPage() {
         depends: [user !== null]
     });
 
+    console.log(data);
+
     if (error) {
         history.push('/');
     }
 
-    const totalPrice = data.reduce((prevItem, curItem) => (prevItem.product.price*prevItem.quantity) + (curItem.product.price*curItem.quantity));
+    let totalPrice = 0;
+    if (!isLoading) {
+        for (let i in data) {
+            let cartItem = data[i];
+            totalPrice += (cartItem.product.price*cartItem.quantity);
+        }
+    }
 
     async function handleToken(token) {
         console.log(token);
         await axios.post(process.env.REACT_APP_BASE_API_URL+"/payment/charge", "", {headers: {
             Authorization: "Bearer "+JSON.parse(localStorage.getItem("user")).token,
             token: token.id,
-            amount: {totalPrice},
+            amount: totalPrice,
         },})
         .then(() => {
            alert("Payment Success");
+           history.push("/products");
         })
         .catch((error) => {
            alert(error);
@@ -36,14 +46,22 @@ function CartPage() {
     }
 
     return (
-        <div className="App">
-            {!isLoading && 
-                <CartContents cart={data} totalPrice={totalPrice} />
-            }
-            <Stripe
-            stripeKey="pk_test_51JPKxyG8YVxXlobJkSvbwGXNpZk1I00iw4PzpdUdC5sWTiqkfOr9pmHTkFrNvhURV90F9O6o7WipZWhAPVwz2Yva00iAQt1xZ4"
-            token={handleToken}
-            />
+        <div>
+            <Header/>
+            <div className="container col-md-5">
+                <h2 className="my-3">Checkout</h2>
+                {!isLoading && 
+                <div>
+                    <CartContents cart={data} totalPrice={totalPrice} />
+                    <div className="d-flex justify-content-end">
+                        <Stripe
+                        stripeKey="pk_test_51JPKxyG8YVxXlobJkSvbwGXNpZk1I00iw4PzpdUdC5sWTiqkfOr9pmHTkFrNvhURV90F9O6o7WipZWhAPVwz2Yva00iAQt1xZ4"
+                        token={handleToken}
+                        />
+                    </div>
+                </div>
+                }
+            </div>
         </div>
     );
 }
